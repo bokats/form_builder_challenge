bfceac81c99a84009b3209faf2a723023b747b73
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-06-25 13:45:42 -0700

    Add navbar listeners

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,13 +4,25 @@ import { hashHistory, router } from 'react-router';
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+    this.changePath = this.changePath.bind(this);
+  }
+
+  changePath(location) {
+    return e => {
+      hashHistory.push(`/${location}`);
+    };
   }
 
   render() {
     return(
-      <div>
-        Navbar Component
-      </div>
+      <nav className='navbar-container'>
+        <div className='navbar-button' onClick={this.changePath('create')}>
+          Create</div>
+        <div className='navbar-button' onClick={this.changePath('preview')}>
+          Preview</div>
+        <div className='navbar-button' onClick={this.changePath('export')}>
+          Export</div>
+      </nav>
     );
   }
 }
